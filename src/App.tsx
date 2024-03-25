d80b5eeb6d68af66e9f324a3e5236ab960b6cc5f
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-03-25 18:56:17 +0530

    resize panels boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+
 export default function App() {
-  return <div className="text-3xl font-bold underline">Hello World!</div>;
+  return (
+    <div className="h-dvh p-2">
+      <PanelGroup direction="horizontal">
+        <Panel>
+          <PanelGroup direction="vertical">
+            <Panel>
+              <div className="border">Code Editor</div>
+            </Panel>
+            <PanelResizeHandle />
+            <Panel>
+              <div className="border">Terminal</div>
+            </Panel>
+          </PanelGroup>
+        </Panel>
+        <PanelResizeHandle />
+        <Panel>
+          <div className="border">Preview</div>
+        </Panel>
+      </PanelGroup>
+    </div>
+  );
 }
