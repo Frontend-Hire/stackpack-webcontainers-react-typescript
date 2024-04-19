abdf042be4e1d47bedd8ce91426c7cf463f25b33
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-04-19 20:03:57 +0530

    final

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -6,11 +6,17 @@ import { FileNode } from '@webcontainer/api';
 import { useWebContainer } from '../../providers/WebContainerProvider/useWebContainer';
 
 export default function CodeEditor() {
-  const { template } = useWebContainer();
+  const { template, webContainer } = useWebContainer();
   const [activeFile, setActiveFile] = React.useState(() => template.entry);
 
   const currentFile = template.files[activeFile] as FileNode;
   const language = getLanguageFromFileName(activeFile);
+
+  const handleCodeChange = async (content: string) => {
+    if (!webContainer) return;
+
+    await webContainer.fs.writeFile(activeFile, content);
+  };
 
   return (
     <div className="h-full">
@@ -22,6 +28,7 @@ export default function CodeEditor() {
       <Editor
         theme="vs-dark"
         path={activeFile}
+        onChange={(value) => handleCodeChange(value ?? '')}
         defaultValue={currentFile.file.contents as string} // Ideally, worry about the encoding in production, for our example, this is fine.
         defaultLanguage={language}
       />
